c2621d5d72a881b3cdcbf925ab899078180e5ec2
Lukas Tveiten <lukastveiten@hotmail.no>
2025-11-22 19:49:14 +0100

    Add multiply_strings/Solution.java (finished)

--- src/multiply_strings/Solution.java
+++ src/multiply_strings/Solution.java
@@ -6,36 +6,36 @@ public class Solution {
 
     public static String multiply(String num1, String num2) {
         int[] result = new int[num1.length() + num2.length()];
-        int i = num1.length() - 1;
-        int j = num2.length() - 1;
-        int carry = 0;
-        while (i >= 0 && j >= 0) {
-            int n1 = num1.charAt(i) - '0';
-            int n2 = num2.charAt(j) - '0';
-            int prod = n1 * n2 + carry;
-            System.out.println(prod);
-            carry = prod / 10;
-            result[i + j + 1] = prod - 10 * carry;
-            if (i == j) {
-                i--;
-                j--;
-            } else if (i > j) {
-                i--;
-            } else {
-                j--;
+
+        for (int i = num1.length() - 1; i >= 0; i--) {
+            int carry = 0;
+            for (int j = num2.length() - 1; j >= 0; j--) {
+                int n1 = num1.charAt(i) - '0';
+                int n2 = num2.charAt(j) - '0';
+                int prod = n1 * n2 + carry + result[i + j + 1];
+                carry = prod / 10;
+                result[i + j + 1] = prod % 10;
+            }
+            if (carry > 0) {
+                result[i] = carry;
             }
         }
-        if (carry > 0) {
-            result[0] = carry;
-        }
-        return Arrays.stream(result)
+        String stringResult = Arrays.stream(result)
+            .dropWhile(num -> num == 0)
             .mapToObj(String::valueOf)
             .reduce("", String::concat);
+        if (!stringResult.isEmpty()) {
+            return stringResult;
+        } else return "0";
     }
 
     static void main() {
         System.out.println(multiply("2", "3"));
         System.out.println(multiply("123", "456"));
+        System.out.println(multiply("1234", "4567"));
+        System.out.println(multiply("2345", "3456"));
+        System.out.println(multiply("99", "99"));
+        System.out.println(multiply("190", "0"));
     }
 
 }
