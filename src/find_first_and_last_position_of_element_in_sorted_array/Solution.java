0d6f9afa4b3a30ee789b2ddbfdf19d0ad562b83e
Lukas Tveiten <lukastveiten@hotmail.no>
2025-11-26 17:38:07 +0100

    Add find_first_and_last_position_in_sorted_array/Solution.java

--- src/find_first_and_last_position_of_element_in_sorted_array/Solution.java
+++ src/find_first_and_last_position_of_element_in_sorted_array/Solution.java
@@ -5,76 +5,41 @@ import java.util.Arrays;
 class Solution {
 
     public static int[] searchRange(int[] nums, int target) {
-        if (nums.length == 0) return new int[] {-1,-1};
-        if (nums[0] == target && nums[nums.length - 1] == target) return new int[] {0,nums.length-1};
+        if (nums.length == 0) return new int[] {-1, -1};
+        if (nums.length == 1) {
+            if (nums[0] == target) {
+                return new int[] {0, 0};
+            } else {
+                return new int[] {-1, -1};
+            }
+        }
+
         int low = 0;
         int high = nums.length - 1;
-        int mid = low + (high - low) / 2;
-        while (high - low > 1) {
-            int num = nums[mid];
-            if (num < target) {
+        int mid = (low + high) / 2;
+        while (nums[mid] != target && nums[mid + 1] != target) {
+            if (nums[mid] < target) {
                 low = mid;
-            } else if (num > target) {
-                high = mid;
-            } else if (mid - 1 < 0 || nums[mid - 1] != target) {
-                break;
             } else {
                 high = mid;
             }
-
-            if (high - low == 1) {
-                if (nums[low] == target) {
-                    mid = low;
-                } else if (nums[high] == target) {
-                    mid = high;
-                } else {
-                    mid = -1;
-                }
-                break;
-            }
-
-            mid = low + (high - low) / 2;
-        }
-
-        int lowResult = -1;
-        if (mid != -1 && nums[mid] == target) lowResult = mid;
-
-        low = 0;
-        high = nums.length - 1;
-        mid = low + (high - low) / 2;
-        while (high - low > 1) {
-            int num = nums[mid];
-            if (num < target) {
-                low = mid;
-            } else if (num > target) {
-                high = mid;
-            } else if (mid + 1 >= nums.length || nums[mid + 1] != target) {
-                break;
+            if (mid != (low + high) / 2) {
+                mid = (low + high) / 2;
             } else {
-                low = mid;
+                return new int[] {-1, -1};
             }
-
-            if (high - low == 1) {
-                if (nums[high] == target) {
-                    mid = high;
-                } else if (nums[low] == target) {
-                    mid = low;
-                } else {
-                    mid = -1;
-                }
-                break;
-            }
-
-            mid = low + (high - low) / 2;
         }
 
-        int highResult = -1;
-        if (mid != -1 && nums[mid] == target) highResult = mid;
+        while (nums[low] != target) low++;
+        while (nums[high] != target) high--;
 
-        return new int[] {lowResult, highResult};
+        return new int[]{low, high};
     }
 
     public static void printResult(int[] nums, int target) {
+        System.out.println("--------------------");
+        System.out.println("nums: " + Arrays.toString(nums));
+        System.out.println("target: " + target);
         System.out.println(Arrays.toString(searchRange(nums, target)));
     }
 
@@ -91,5 +56,6 @@ class Solution {
         printResult(new int[] {0,0,0}, 0);
         printResult(new int[] {0,0,0,1}, 1);
         printResult(new int[] {0,0,0,1,1}, 1);
+        printResult(new int[] {0,1,1,1,1}, 0);
     }
 }
